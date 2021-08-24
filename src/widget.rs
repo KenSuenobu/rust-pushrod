478a5f804c6df14dd22b7b3e2b287a270d69a205
Kenji Suenobu <kenji@Kenjis-iMac.attlocal.net>
2021-08-23 20:36:38 -0500

    save

--- src/widget.rs
+++ src/widget.rs
@@ -1,3 +1,4 @@
+// Widget
 // Pushrod
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -11,7 +12,3 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
-pub mod texture;
-pub mod geometry;
-pub mod widget;
